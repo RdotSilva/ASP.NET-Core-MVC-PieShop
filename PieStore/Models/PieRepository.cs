162a976fe3648f9e8564d4cda5547660671bf847
Ryan Silva <RyandotSilva@gmail.com>
2019-10-04 19:12:07 -0400

    Update PieRepository properties

--- PieStore/Models/PieRepository.cs
+++ PieStore/Models/PieRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PieStore.Models
 {
@@ -14,5 +15,18 @@ namespace PieStore.Models
             _appDbContext = appDbContext;
         }
 
+        public IEnumerable<Pie> AllPies
+        {
+            get { return _appDbContext.Pies.Include(c => c.Category); }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get { return _appDbContext.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek); }
+        }
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
     }
 }
