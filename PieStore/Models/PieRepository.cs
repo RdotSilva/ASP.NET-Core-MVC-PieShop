25cb025f29dd66c4a02c878944443846ede634be
Ryan Silva <RyandotSilva@gmail.com>
2019-10-04 19:06:14 -0400

    Add PieRepository class

---
+++ PieStore/Models/PieRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieStore.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+    }
+}
