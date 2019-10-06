dc6de9102880b84cdf31c72c29716332cfd9836b
Ryan Silva <RyandotSilva@gmail.com>
2019-10-06 10:35:19 -0400

    Add ShoppingCartSummary class

---
+++ PieStore/Components/ShoppingCartSummary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieStore.Models;
+
+namespace PieStore.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+    }
+}
