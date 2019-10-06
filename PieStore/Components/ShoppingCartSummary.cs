ecc830949f838461b2b347543aceaa884174f3c5
Ryan Silva <RyandotSilva@gmail.com>
2019-10-06 10:39:10 -0400

    Add Invoke method

--- PieStore/Components/ShoppingCartSummary.cs
+++ PieStore/Components/ShoppingCartSummary.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PieStore.Models;
+using PieStore.ViewModels;
 
 namespace PieStore.Components
 {
@@ -14,6 +15,21 @@ namespace PieStore.Components
         public ShoppingCartSummary(ShoppingCart shoppingCart)
         {
             _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+
+            };
+            return View(shoppingCartViewModel);
         }
     }
 }
