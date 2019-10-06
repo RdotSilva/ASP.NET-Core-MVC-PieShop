271893af42493fc1832c40d238b7143eb3cbea08
Ryan Silva <RyandotSilva@gmail.com>
2019-10-06 12:45:53 -0400

    Add authorization logic

--- PieStore/Controllers/OrderController.cs
+++ PieStore/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PieStore.Models;
 
@@ -9,6 +10,7 @@ using PieStore.Models;
 
 namespace PieStore.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IOrderRepository _orderRepository;
--- PieStore/Startup.cs
+++ PieStore/Startup.cs
@@ -60,6 +60,7 @@ namespace PieStore
 
             app.UseRouting();
             app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
